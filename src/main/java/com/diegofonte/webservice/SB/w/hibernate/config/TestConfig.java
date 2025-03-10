8c7e6e2284d129dd4ea8930b227c81b8808b6394
Diego Fonte <diegofonteboacruz@gmail.com>
2025-03-10 15:45:14 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/diegofonte/webservice/SB/w/hibernate/config/TestConfig.java
@@ -0,0 +1,30 @@
+package com.diegofonte.webservice.SB.w.hibernate.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.diegofonte.webservice.SB.w.hibernate.entities.User;
+import com.diegofonte.webservice.SB.w.hibernate.repositories.UserRepository;
+
+@Configuration
+@Profile("testdb")
+public class TestConfig implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+	}
+	
+
+}
---
+++ src/main/java/com/diegofonte/webservice/SB/w/hibernate/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.diegofonte.webservice.SB.w.hibernate.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.diegofonte.webservice.SB.w.hibernate.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
