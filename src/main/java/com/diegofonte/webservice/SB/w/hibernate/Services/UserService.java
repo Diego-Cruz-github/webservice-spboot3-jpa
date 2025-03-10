5fb0df3ea74d71f97a641ed96254c7999df70317
Diego Fonte <diegofonteboacruz@gmail.com>
2025-03-10 16:33:47 -0300

    Service layer, component registration

---
+++ src/main/java/com/diegofonte/webservice/SB/w/hibernate/Services/UserService.java
@@ -0,0 +1,27 @@
+package com.diegofonte.webservice.SB.w.hibernate.Services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegofonte.webservice.SB.w.hibernate.entities.User;
+import com.diegofonte.webservice.SB.w.hibernate.repositories.UserRepository;
+
+@Service 
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
--- src/main/java/com/diegofonte/webservice/SB/w/hibernate/repositories/UserRepository.java
+++ src/main/java/com/diegofonte/webservice/SB/w/hibernate/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.diegofonte.webservice.SB.w.hibernate.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
