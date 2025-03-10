67b63e5143b46eb140e4ac2f70f971b0c42de257
Diego Fonte <diegofonteboacruz@gmail.com>
2025-03-10 13:34:05 -0300

    Project created

---
+++ src/main/java/com/diegofonte/webservice/SB/w/hibernate/WebserviceSbWHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.diegofonte.webservice.SB.w.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceSbWHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceSbWHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegofonte/webservice/SB/w/hibernate/WebserviceSbWHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegofonte.webservice.SB.w.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceSbWHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
